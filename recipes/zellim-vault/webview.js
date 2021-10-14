e717944134f77a4be6d6ef65018ec0e9e962efd9
Sidney Rodrigues <sidney@feracode.com>
2021-10-14 11:55:51 -0600

    Change of Icons and Mute Z Mods

--- recipes/zellim-vault/webview.js
+++ recipes/zellim-vault/webview.js
@@ -1,8 +1,1 @@
-module.exports = Ferdi => {
-  const getMessages = () => {
-    const unread = document.querySelector("[data-service-notification-count=general]")?.textContent?.trim() || '0';
-    Ferdi.setBadge(Ferdi.safeParseInt(unread));
-  };
-
-  Ferdi.loop(getMessages);
-};
+module.exports = () => {};
